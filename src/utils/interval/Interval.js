237041f19684dfef7a83f187e796b970aaffa80f
Pavel Spacil <pavel.spacil@gmail.com>
2022-04-12 10:49:27 +0200

    feat: create `Interval` class

---
+++ src/utils/interval/Interval.js
@@ -0,0 +1,110 @@
+/**
+ * Class for handling repeated function calls.
+ */
+export class Interval {
+    constructor({ interval = 3000 } = {}) {
+        // Keys are codes, values are objects { id: number, paused: boolean }
+        this._intervals = {};
+        // Default interval interval
+        this._interval = interval;
+        // All intervals are paused
+        this._paused = false;
+    }
+
+    destroy() {
+        this.stopAll();
+    }
+
+    /**
+     * @param {string} code
+     * @param {function} callback
+     * @param {number} [interval] In milliseconds
+     */
+    start(code, callback, interval = this._interval) {
+        if (typeof callback === 'function') {
+            this.stop(code);
+
+            const id = setInterval(() => {
+                const interval = this._getIntervalByCode(code);
+
+                if (!interval || !interval.paused) {
+                    callback();
+                }
+            }, interval);
+
+            this._intervals[code] = {
+                id,
+                paused: this._paused,
+            };
+        } else {
+            throw new Error(`'callback' is not a function`);
+        }
+    }
+
+    /**
+     * @param {string} code
+     */
+    stop(code) {
+        const interval = this._getIntervalByCode(code);
+
+        if (interval) {
+            clearInterval(interval.id);
+            delete this._intervals[code];
+        }
+    }
+
+    stopAll() {
+        Object.keys(this._intervals).forEach((_code) => {
+            this.stop(_code);
+        });
+    }
+
+    /**
+     * @param {string} code
+     */
+    pause(code) {
+        const interval = this._getIntervalByCode(code);
+
+        if (interval) {
+            interval.paused = true;
+        }
+    }
+
+    pauseAll() {
+        this._paused = true;
+
+        Object.keys(this._intervals).forEach((code) => {
+            this.pause(code);
+        });
+    }
+
+    /**
+     * @param {string} code
+     */
+    resume(code) {
+        const interval = this._getIntervalByCode(code);
+
+        if (interval) {
+            interval.paused = false;
+        }
+    }
+
+    resumeAll() {
+        this._paused = false;
+
+        Object.keys(this._intervals).forEach((code) => {
+            this.resume(code);
+        });
+    }
+
+    /**
+     * @param code
+     * @return {*|null}
+     * @private
+     */
+    _getIntervalByCode(code) {
+        const intervals = this._intervals;
+
+        return code in intervals ? intervals[code] : null;
+    }
+}
---
+++ src/utils/interval/Interval.spec.js
@@ -0,0 +1,206 @@
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
+import { Interval } from './Interval.js';
+
+let interval = null;
+let fn = null;
+
+function destroyInterval() {
+    if (interval) {
+        interval.destroy();
+        interval = null;
+    }
+}
+
+beforeEach(() => {
+    vi.useFakeTimers();
+    fn = vi.fn();
+    interval = new Interval();
+});
+
+afterEach(() => {
+    destroyInterval();
+
+    vi.restoreAllMocks();
+    fn = null;
+});
+
+describe('Interval', () => {
+    it('should be able to start calling given function repeatedly', () => {
+        interval.start('code', fn, 100);
+
+        vi.advanceTimersByTime(200);
+
+        expect(fn).toBeCalledTimes(2);
+    });
+
+    it('should throw an exception if interval is started and given function is not a function', () => {
+        expect(() => {
+            interval.start('code');
+        }).toThrow();
+    });
+
+    it('should be able to start calling given function repeatedly by default amount of milliseconds, if `interval` argument is not given', () => {
+        destroyInterval();
+
+        interval = new Interval({
+            interval: 100,
+        });
+
+        interval.start('code', fn);
+
+        vi.advanceTimersByTime(200);
+
+        expect(fn).toBeCalledTimes(2);
+    });
+
+    it('should be able to stop calling function by given code', () => {
+        interval.start('code', fn, 100);
+
+        vi.advanceTimersByTime(200);
+
+        interval.stop('code');
+
+        vi.advanceTimersByTime(200);
+
+        expect(fn).toBeCalledTimes(2);
+    });
+
+    it('should be able to pause calling function by given code', () => {
+        interval.start('code', fn, 100);
+
+        vi.advanceTimersByTime(200);
+
+        interval.pause('code');
+
+        vi.advanceTimersByTime(200);
+
+        expect(fn).toBeCalledTimes(2);
+    });
+
+    it('should be able to resume calling function by given code', () => {
+        interval.start('code', fn, 100);
+
+        vi.advanceTimersByTime(200);
+
+        interval.pause('code');
+
+        vi.advanceTimersByTime(500);
+
+        interval.resume('code');
+
+        vi.advanceTimersByTime(100);
+
+        expect(fn).toBeCalledTimes(3);
+    });
+
+    it('should properly handle multiple intervals', () => {
+        const fn2 = vi.fn();
+
+        interval.start('code1', fn, 100);
+        interval.start('code2', fn2, 200);
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(4);
+        expect(fn2).toBeCalledTimes(2);
+
+        interval.pause('code1');
+        interval.pause('code2');
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(4);
+        expect(fn2).toBeCalledTimes(2);
+
+        interval.resume('code1');
+        interval.resume('code2');
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(8);
+        expect(fn2).toBeCalledTimes(4);
+
+        interval.stop('code1');
+        interval.stop('code2');
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(8);
+        expect(fn2).toBeCalledTimes(4);
+    });
+
+    it('should stop all intervals by calling `stopAll` method', () => {
+        const fn2 = vi.fn();
+
+        interval.start('code1', fn, 100);
+        interval.start('code2', fn2, 200);
+
+        vi.advanceTimersByTime(400);
+
+        interval.stopAll();
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(4);
+        expect(fn2).toBeCalledTimes(2);
+    });
+
+    it('should pause all intervals by calling `pauseAll` method', () => {
+        const fn2 = vi.fn();
+
+        interval.start('code1', fn, 100);
+        interval.start('code2', fn2, 200);
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(4);
+        expect(fn2).toBeCalledTimes(2);
+
+        interval.pauseAll();
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(4);
+        expect(fn2).toBeCalledTimes(2);
+    });
+
+    it('should pause all intervals when `pauseAll` method was called before intervals were started', () => {
+        const fn2 = vi.fn();
+
+        interval.pauseAll();
+
+        interval.start('code1', fn, 100);
+        interval.start('code2', fn2, 200);
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(0);
+        expect(fn2).toBeCalledTimes(0);
+    });
+
+    it('should resume all intervals by calling `resumeAll` method', () => {
+        const fn2 = vi.fn();
+
+        interval.start('code1', fn, 100);
+        interval.start('code2', fn2, 200);
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(4);
+        expect(fn2).toBeCalledTimes(2);
+
+        interval.pauseAll();
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(4);
+        expect(fn2).toBeCalledTimes(2);
+
+        interval.resumeAll();
+
+        vi.advanceTimersByTime(400);
+
+        expect(fn).toBeCalledTimes(8);
+        expect(fn2).toBeCalledTimes(4);
+    });
+});
