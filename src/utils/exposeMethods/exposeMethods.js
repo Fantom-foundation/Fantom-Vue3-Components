03e25965d4a732f0910635311a653d842e26e832
Pavel Spacil <pavel.spacil@gmail.com>
2022-06-20 08:14:53 +0200

    feat: create `exposeMethods` utility function

---
+++ src/utils/exposeMethods/exposeMethods.js
@@ -0,0 +1,25 @@
+import { isRef, unref } from 'vue';
+
+/**
+ * Expose vue compoent's methods
+ *
+ * @param {Object} component
+ * @param {string[]} methods
+ * @return {{}} Keys are method names, values are functions
+ */
+export function exposeMethods(component, methods = []) {
+    let value = {};
+
+    methods.forEach((method) => {
+        value[method] = function (...args) {
+            const comp = isRef(component) ? unref(component) : component;
+            const fn = comp ? comp[method] : null;
+
+            if (typeof fn === 'function') {
+                fn(...args);
+            }
+        };
+    });
+
+    return value;
+}
---
+++ src/utils/exposeMethods/exposeMethods.spec.js
@@ -0,0 +1,34 @@
+import { describe, it, expect, vi } from 'vitest';
+import { exposeMethods } from '@/utils/exposeMethods/exposeMethods.js';
+import { ref } from 'vue';
+
+describe('exposeMethods', () => {
+    it('should return object where keys are given method names and values are functions', () => {
+        const component = ref({
+            method: () => {},
+        });
+
+        const value = exposeMethods(component, ['method']);
+
+        expect(typeof value.method === 'function').toBe(true);
+    });
+
+    it("should call given component's methods", () => {
+        const method1 = vi.fn(() => {});
+        const method2 = vi.fn(() => {});
+        const method3 = vi.fn(() => {});
+        const component = ref({
+            method1,
+            method2,
+        });
+
+        const value = exposeMethods(component, ['method1', 'method2', 'method3']);
+
+        value.method1();
+        value.method2('foo');
+
+        expect(method1).toHaveBeenCalled();
+        expect(method2).toHaveBeenCalledWith('foo');
+        expect(method3).not.toHaveBeenCalled();
+    });
+});
