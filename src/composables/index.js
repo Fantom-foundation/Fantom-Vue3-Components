8d92665bf357f3ae3c9ab2045dc2c05674b837ec
Pavel Spacil <pavel.spacil@gmail.com>
2022-08-02 10:27:04 +0200

    chore: re-export new composables

--- src/composables/index.js
+++ src/composables/index.js
@@ -3,3 +3,5 @@ export * from './usePageVisibility/usePageVisibility.js';
 export * from './useInterval/useInterval.js';
 export * from './useNotifications/useNotifications.js';
 export * from './useMethods/useMethods.js';
+export * from './useFWindow/useFWindow.js';
+export * from './useFPopover/useFPopover.js';
